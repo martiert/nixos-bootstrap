12e84f1ebb3faeb78f66e09a1bf20d69861a1d32
Martin Ertsås <martiert@gmail.com>
2024-02-13 13:17:56 +0100

    Add a default bootstrap

--- flake.nix
+++ flake.nix
@@ -14,9 +14,118 @@
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
     };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, module, home-manager, nixos-hardware }: {
+  outputs = { self, nixpkgs, module, agenix, home-manager, nixos-hardware }: {
+    nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        home-manager.nixosModules.home-manager
+        module.nixosModules.default
+        agenix.nixosModules.default
+        ({pkgs, config, ...}: {
+          system.stateVersion = "23.05";
+
+          environment.variables.EDITOR = "vim";
+          environment.variables.MOZ_ENABLE_WAYLAND = "1";
+          environment.systemPackages = [ agenix.packages."x86_64-linux".default ];
+
+          networking.hostName = "perrin";
+          age.secrets."wpa_supplicant_enp6s0".file = ../../secrets/wpa_supplicant_wired.age;
+
+          martiert = {
+            system = {
+              type = "server";
+              gpu = "amd";
+            };
+            mountpoints = {
+              root = {
+                encryptedDevice = "/dev/disk/by-uuid/34185190-271f-464b-91aa-d6707835ab60";
+                device = "/dev/disk/by-uuid/29a4d0df-3ec4-4b32-914a-9329d4b18c99";
+                credentials = [
+                  "7a1b48e5df7fe3f91fc7b44a5404a6a2"
+                  "629a8ce0e10987d16ea20dc186aac48c"
+                  "1b633076d0cef092511ad5beca0ab1c5"
+                ];
+              };
+              boot = "/dev/disk/by-uuid/34F2-B158";
+              swap = "/dev/disk/by-partuuid/1bc95ed3-d38e-d64e-9410-43067e6cd4d5";
+            };
+            boot = {
+              initrd.extraAvailableKernelModules = [ "usbhid" ];
+              efi.removable = true;
+            };
+            networking = {
+              dhcpcd.leaveResolveConf = true;
+              interfaces = {
+                "eno1" = {
+                  enable = true;
+                  useDHCP = true;
+                };
+                "enp6s0" = {
+                  enable = true;
+                  useDHCP = true;
+                  staticRoutes = true;
+                  supplicant = {
+                    enable = true;
+                    wired = true;
+                    configFile = config.age.secrets.wpa_supplicant_enp6s0.path;
+                  };
+                };
+              };
+              tables = {
+                cisco = {
+                  number = 42;
+                  enable = true;
+                  rules = [
+                    {
+                      from = "192.168.1.1/24";
+                    }
+                  ];
+                  routes = {
+                    default = {
+                      value = "via 192.168.1.1";
+                    };
+                  };
+                };
+              };
+            };
+            sshd = {
+              enable = true;
+            };
+          };
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.martin = { lib, config, osConfig, ... }: {
+              imports = [
+                module.nixosModules.home-manager
+              ];
+              config = {
+                martiert = lib.mkDefault osConfig.martiert;
+                home.stateVersion = osConfig.system.stateVersion;
+              };
+            };
+          };
+
+          nix = {
+            registry.nixpkgs.flake = nixpkgs;
+            package = pkgs.nixUnstable;
+            extraOptions = ''
+              keep-outputs = true
+              keep-derivations = true
+              experimental-features = nix-command flakes
+            '';
+          };
+        })
+      ];
+    };
     nixosConfigurations.pinarello = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
 
---
+++ secrets.nix
@@ -0,0 +1,34 @@
+let
+  moridin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDczcJT6Dw1drNAjK9AeehtE+UUfVPJp/3ud2RESyEZ6 root@moridin";
+  perrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBGRf2h4CTeeSWRsozsKAJOstTDNi06NXe/n+GAkQ/K root@Perrin";
+  aginor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRjg84Y2jgL/qFgc0BPnZvjLkN/fnsDTdLyFfbK+KmZ root@Aginor";
+  moghedien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlDQcKcShLLVDXOLzzHKx7D6gNetKxC2nL7nFz6SWtu root@moghedien";
+  mattrim = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFeYhxfQEqydAEBn9Dw8REkAcBYLc7h+l7CW9QtLjDl+ root@mattrim";
+  schnappi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDA40XbaYVw5sQN25PuEnfahpb4OO3XChh53jk18zkIg root@schnappi";
+  pinarello = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzO9jNL7DNqmz5WPUWe+PceGFUxQV0svBo4uSiacr6b root@pinarello";
+
+  octoprint = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUplHY8ALir2FCM4dTQlH0L17dhkjxiNhq6p79h1nP5 root@octoprint";
+  pihole = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMizRj3YEyAbCL3T9S8Fa2IvSN2Ia/U1hD2ItEzALhZI root@pihole";
+  tmate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtQnvgjC+fwdv9mLJiWa+PrapWmFvOidO0pxVUPnPm5 root@tmate";
+  foundry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILBSzOZjEk6huvgwj3K+ycCTgSBxYKaxQVHpLd/cRTwH root@foundry";
+  vpnrouter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPN3+YyY/rqzCAa8PZUf4au4ZkzG5QlN+TJQ8xxzxSe root@vpnrouter";
+  hydra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwwGaFWQiFilcX2kYjPZWpr/6ORZ8dyMzYcwL+sLFWP root@virtualbox";
+
+  editKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElUoVKWA9hI7T4WIRZZfwZl8+u86/PsewtRJc25ZhKK martin@moridin"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtv8ELid3Rl95HFtmE55gXyZiO0Hh3RNmwnDYVU2Szb martin@perrin"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHTWbjpb92qz5YurxsbdV2PzjdN8TZh4S91AXqYCU1s martin@moghedien"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILuFi4JSDhCe/aqTxoL5zt8r45laYRq+kEvGNRDd8REW martin@aginor"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRw8XCxKsXXpmEvWGdP/edHvfcNhRTnmj/rrdNp+cqM martin@schnappi"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVZ0SDtC9spMJSAvPyhTfemoqBM3BWstsp5HxmWvXUI martin@pinarello"
+  ];
+in {
+  "secrets/wpa_supplicant_wired.age".publicKeys = [ perrin moridin ] ++ editKeys;
+  "secrets/wpa_supplicant_wireless.age".publicKeys = [ moghedien octoprint schnappi pinarello ] ++ editKeys;
+  "secrets/dns_servers.age".publicKeys = [ perrin moridin ] ++ editKeys;
+  "secrets/citrix.age".publicKeys = [ perrin aginor ] ++ editKeys;
+  "secrets/mattrim_dropbear_key.age".publicKeys = [ mattrim ] ++ editKeys;
+  "secrets/vpn_passphrase.age".publicKeys = [ vpnrouter ] ++ editKeys;
+  "secrets/nordvpn_credentials.age".publicKeys = [ vpnrouter ] ++ editKeys;
+  "secrets/hydra_private_key.age".publicKeys = [ hydra mattrim ] ++ editKeys;
+}
